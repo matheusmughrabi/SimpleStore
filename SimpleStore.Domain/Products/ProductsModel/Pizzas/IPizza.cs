f4f64f8da34e8f9b5b411ac498da314b259193bf
Matheus Campanini Mughrabi <matheus.mughrabi@gmail.com>
2020-12-16 17:31:23 -0300

    - Adds SimpleStore.Domain, SimpleStore.DataAccessLayer and SimpleStore.ConsoleUI - I have developed these three projects in the past few days using another repository, but decided to start thing up here in this repository (the other was a bit messy in terms of commits and now I'm gonna try to be a little more organized and follow gitflow)

---
+++ SimpleStore.Domain/Products/ProductsModel/Pizzas/IPizza.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleStore.Domain.Products.Models.Pizzas
+{
+    public interface IPizza : IBaseProduct
+    {
+        string Type { get; set; }
+        string Sauce { get; set; }
+    }
+}
---
+++ SimpleStore.Domain/Stores/StoresModel/PizzaStores/PizzaStoreModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleStore.Domain.Stores.Models.PizzaStores
+{
+    public class PizzaStoreModel : IPizzaStore
+    {
+        public int Id { get; set; }
+        public string StoreName { get; set; }
+    }
+}
