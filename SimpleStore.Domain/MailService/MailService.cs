d220fe556153bbe413a853faaf0af67cd22a72e9
Matheus Campanini Mughrabi <matheus.mughrabi@gmail.com>
2020-12-28 18:10:41 -0300

    Checks if the provided email is valid

--- SimpleStore.Domain/MailService/MailService.cs
+++ SimpleStore.Domain/MailService/MailService.cs
@@ -20,8 +20,27 @@ namespace SimpleStore.Domain.MailService
             Configuration = builder.Build();
         }
 
+        private static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
         public static bool SendMail(string toUsername, string toEmail, string subject, string body)
         {
+            bool isValidEmail = IsValidEmail(toEmail);
+            if (isValidEmail == false)
+            {
+                return false;
+            }
+
             GetConfigurationSettings();
 
             var mailMessage = new MimeMessage();
