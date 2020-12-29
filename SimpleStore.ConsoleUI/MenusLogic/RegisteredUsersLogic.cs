4b5924c001beb8d613579b387666d216cbf212fc
Matheus Campanini Mughrabi <matheus.mughrabi@gmail.com>
2020-12-29 12:22:32 -0300

    Creates managerRegisteredUsersMenu, RegisteredUsersLogic and RegisteredUsersInfo

---
+++ SimpleStore.ConsoleUI/MenusLogic/RegisteredUsersLogic.cs
@@ -0,0 +1,33 @@
+﻿using SimpleStore.Domain.Manager.ManagerOperations.Interfaces;
+using SimpleStore.Domain.UsersAuthenticator.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleStore.ConsoleUI.MenusLogic
+{
+    public class RegisteredUsersLogic
+    {
+        private readonly IRegisteredUsersInfo _registeredUsersInfo;
+
+        public RegisteredUsersLogic(IRegisteredUsersInfo registeredUsersInfo)
+        {
+            _registeredUsersInfo = registeredUsersInfo;
+        }
+
+        public bool DisplayRegisteredUsers(List<string> inputs)
+        {
+            List<UserModel> registeredUsers = _registeredUsersInfo.GetRegisteredUsers();
+
+            foreach (var user in registeredUsers)
+            {
+                Console.WriteLine($"{ user.FullName } || { user.Email } || { user.Username } ");
+            }
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+
+            return true;
+        }
+    }
+}
---
+++ SimpleStore.Domain/Manager/ManagerOperations/RegisteredUsersInfo.cs
@@ -0,0 +1,25 @@
+﻿using SimpleStore.Domain.Manager.ManagerOperations.Interfaces;
+using SimpleStore.Domain.Services.AuthenticationServices;
+using SimpleStore.Domain.UsersAuthenticator.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleStore.Domain.Manager.ManagerOperations
+{
+    public class RegisteredUsersInfo : IRegisteredUsersInfo
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public RegisteredUsersInfo(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        public List<UserModel> GetRegisteredUsers()
+        {
+            List<UserModel> registeredUsers = _authenticationService.GetRegisteredUsers();
+            return registeredUsers;
+        }
+    }
+}
