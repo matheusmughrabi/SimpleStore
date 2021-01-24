8938e8263184c93850a1c771c825019d81cfc0d8
Matheus Campanini Mughrabi <matheus.mughrabi@gmail.com>
2021-01-24 11:07:26 -0300

    - Creates Repository and IRepository

---
+++ SimpleStore.DataAccessLayer/Data/Repository/RepositorySPCallSqlServer.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using SimpleStore.Domain.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace SimpleStore.DataAccessLayer.Data.Repository
+{
+    public class RepositorySPCallSqlServer : IRepositorySPCall
+    {
+        private readonly string _connectionString = ConfigurationManager.ConnectionStrings["sqlServerExtFactory"].ConnectionString;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> ExecuteReturnList<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlConn = new SqlConnection(_connectionString))
+            {
+                sqlConn.Open();
+
+                IEnumerable<T> scalerList = sqlConn.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+
+                return scalerList;
+            }
+        }
+
+        public T ExecuteReturnScaler<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlConn = new SqlConnection(_connectionString))
+            {
+                sqlConn.Open();
+
+                T scaler = (T)Convert.ChangeType(sqlConn.ExecuteScalar<T>(procedureName, param, commandType: CommandType.StoredProcedure), typeof(T));
+
+                return scaler;
+            }
+        }
+
+        public void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlConn = new SqlConnection(_connectionString))
+            {
+                sqlConn.Open();
+
+                sqlConn.Execute(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
---
+++ SimpleStore.Domain/IRepository/IRepositorySPCall.cs
@@ -0,0 +1,16 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleStore.Domain.IRepository
+{
+    public interface IRepositorySPCall : IDisposable
+    {
+        T ExecuteReturnScaler<T>(string procedureName, DynamicParameters param = null);
+
+        void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null);
+
+        IEnumerable<T> ExecuteReturnList<T>(string procedureName, DynamicParameters param = null);
+    }
+}
